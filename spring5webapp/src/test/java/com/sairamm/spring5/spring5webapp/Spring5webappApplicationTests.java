1865d40476431352bd7d0c6acc13fdfdbcb76b69
Sairam Madem <smadem@ptc.com>
2020-10-11 20:51:09 -0400

    my first spring5 web app

---
+++ spring5webapp/src/test/java/com/sairamm/spring5/spring5webapp/Spring5webappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sairamm.spring5.spring5webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5webappApplicationTests {
+
+	@Test
+	void contextLoads() {
+		
+	}
+}
